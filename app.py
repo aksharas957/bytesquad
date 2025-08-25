0985d5899f1ec4834fc76de18a248f919b7bc42e
Akshara Swaminathan <akshisheep@gmail.com>
2025-08-24 17:03:48 -0700

    Updated colors, font, and made table

--- app.py
+++ app.py
@@ -7,40 +7,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# Home route
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# MySQL configuration
-# db_config = {
-#     'host': '127.0.0.1',
-#     'user': 'root',
-#     'password': 'bytesquad',
-#     'database': 'bytesquad'
-# }
 
-# # Create a MySQL connection
-# def get_db_connection():
-#     return mysql.connector.connect(**db_config)
-
-
-# @app.route('/show')
-# def show_users():
-#     conn = get_db_connection()
-#     cursor = conn.cursor(dictionary=True)  # dictionary=True gives dicts instead of tuples
-#     cursor.execute("SELECT * FROM members")
-#     users = cursor.fetchall()
-#     cursor.close()
-#     conn.close()
-    
-#     return render_template('table.html',data=users)
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
-# Replace with your actual Neon credentials
 DB_URL ='postgresql://neondb_owner:npg_PGBFmEw0D8xV@ep-damp-cherry-aegep02p-pooler.c-2.us-east-2.aws.neon.tech/bytesquad-website?sslmode=require&channel_binding=require'
 
 
